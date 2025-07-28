a66347769b64a67ac109be6cf80461d586177b94
Mrunal Nalawade <mrunalawade14@gmail.com>
2025-07-28 21:10:08 +0530

    cors frontend url

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const cartRoutes = require('./routes/cartRoutes');
 dotenv.config();
 
 const app = express();
-app.use(cors()); 
+app.use(cors({ origin: 'https://ecomm-frontend-1xqc.onrender.com' })); 
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
